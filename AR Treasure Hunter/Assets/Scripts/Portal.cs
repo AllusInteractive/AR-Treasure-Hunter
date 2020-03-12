908cb18e5d210ef069da3cf480903f62e670c3a4
AllusInteractive <AllusInteractive@users.noreply.github.com>
2020-03-12 11:38:18 +0000

    Created script for the portal door

---
+++ AR Treasure Hunter/Assets/Scripts/Portal.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Portal : MonoBehaviour
+{
+    public Material[] materials;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider collider)
+    {
+        if (collider.name != "Main Camera")
+        {
+            return;
+        }
+
+        // Outside the portal
+        if (transform.position.z > collider.transform.position.z)
+        {
+            foreach (var mat in materials)
+            {
+                mat.SetInt("stest", (int)CompareFunction.Equal);
+            }
+        }
+        // Inside the portal
+        else
+        {
+            foreach (var mat in materials)
+            {
+                mat.SetInt("stest", (int)CompareFunction.NotEqual);
+            }
+        }
+    }
+}
