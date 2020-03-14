202b3a2e6d6f9c4fbcdbe0bda65d7f6a631a3463
AllusInteractive <AllusInteractive@users.noreply.github.com>
2020-03-14 21:25:19 +0000

    Added basic menu scene.

---
+++ AR Treasure Hunter/Assets/Scripts/Menu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Build Index:
+    // 0 - Menu
+    // 1 - Basic Portal Scene
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void GoToBasicPortalScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
